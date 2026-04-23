65becca54183358fe51a28f666ad1e7d07b927b6
Haseeb04092002 <163792769+Haseeb04092002@users.noreply.github.com>
2026-04-23 02:39:18 -0700

    up

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/oes-store/';
-// $config['base_url'] = 'https://oxbridgeeducationalservices.com/';
+// $config['base_url'] = 'http://localhost/oes-store/';
+$config['base_url'] = 'https://store.oxbridgeeducationalservices.com/';
 
 /*
 |--------------------------------------------------------------------------
